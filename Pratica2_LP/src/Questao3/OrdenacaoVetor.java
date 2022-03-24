549ee6ee7704cfeac9dc7d23b9f02b225365008a
YuriMeloS <65675262+YuriMeloS@users.noreply.github.com>
2022-03-23 22:29:56 -0300

    Add files via upload

--- Pratica2_LP/src/Questao3/OrdenacaoVetor.java
+++ Pratica2_LP/src/Questao3/OrdenacaoVetor.java
@@ -1,10 +1,31 @@
 package Questao3;
+import java.util.Scanner;
 
 public class OrdenacaoVetor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner ler = new Scanner(System.in); 
+		int[] vetor = new int[10];
+		int aux=0;
+		for(int i=0; i<10; i++) {
+			System.out.printf("\n Digite a posição %d:",i+1);
+			vetor[i] = ler.nextInt();
+		}
+		
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<9; j++) {
+				if(vetor[j] > vetor[j+1]) {
+					aux = vetor[j];
+					vetor[j] = vetor[j+1];
+					vetor[j+1] = aux;
+				}
+			}
+		}
+		System.out.println("Vetor Ordenado");
+		for(int i=0; i<10; i++) {
+			System.out.printf("%d\t", vetor[i]);
+		}
+		
 	}
 
 }
